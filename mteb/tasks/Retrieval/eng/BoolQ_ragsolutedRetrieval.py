f6621669f45e4202dc57b93d91e5096582d51406
yjoonjang <yjoonjang34@gmail.com>
2025-02-10 20:47:11 +0900

    fix imports

--- mteb/tasks/Retrieval/eng/BoolQ_ragsolutedRetrieval.py
+++ mteb/tasks/Retrieval/eng/BoolQ_ragsolutedRetrieval.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
+from mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from .......mteb.mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
 
 class BoolQ_ragsolutedRetrieval(AbsTaskRetrieval):
