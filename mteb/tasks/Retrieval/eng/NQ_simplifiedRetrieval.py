8a11f3347cb360aabe9b2e0a9c9be74dd58923f6
yjoonjang <yjoonjang34@gmail.com>
2025-02-11 01:16:21 +0900

    add dev

--- mteb/tasks/Retrieval/eng/NQ_simplifiedRetrieval.py
+++ mteb/tasks/Retrieval/eng/NQ_simplifiedRetrieval.py
@@ -17,7 +17,7 @@ class NQ_simplifiedRetrieval(AbsTaskRetrieval):
         type="Retrieval",
         category="s2p",
         modalities=["text"],
-        eval_splits=["test"],
+        eval_splits=["dev"],
         eval_langs=["eng-Latn"],
         main_score="ndcg_at_10",
         date=None,
